81376cafcc5e1a768af85f8cf5d4eb570cf33112
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-10-25 23:42:33 +0300

    simple test

---
+++ test/fixtures/selector-simple.js
@@ -0,0 +1,35 @@
+module.exports = {
+    start: 0,
+    end: 12,
+    type: 'stylesheet',
+    content: [
+        {
+            type: 'ruleset',
+            start: 0,
+            end: 11,
+            block: {
+                start: 4,
+                end: 10,
+                type: 'block',
+                declarations: [
+                    {
+                        start: 4,
+                        end: 9,
+                        type: 'declaration',
+                        property: {
+                            start: 4,
+                            end: 8,
+                            name: 'color',
+                            type: 'property'
+                        }
+                    }
+                ]
+            },
+            selector: {
+                start: 0,
+                end: 2,
+                type: 'selector'
+            }
+        }
+    ]
+};
