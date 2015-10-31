cf4742658ed7d0fdcbad3a6c39d4350cfd814518
Roman Dvornov <rdvornov@gmail.com>
2015-10-31 17:09:35 +0300

    use lists for content instead of arrays

--- test/fixtures/parser/complex-value.js
+++ test/fixtures/parser/complex-value.js
@@ -1,29 +1,33 @@
 module.exports = {
     type: 'stylesheet',
     start: 0,
-    length: 21,
-    content: [
-        {
-            type: 'ruleset',
+    length: 25,
+    content: {
+        type: 'ruleset',
+        start: 0,
+        length: 25,
+        next: null,
+
+        selector: {
+            type: 'selector',
             start: 0,
+            length: 2
+        },
+        block: {
+            type: 'block',
+            start: 3,
             length: 21,
-            selector: {
-                type: 'selector',
-                start: 0,
-                length: 2
-            },
-            block: {
-                type: 'block',
+            content: {
+                type: 'declaration',
                 start: 3,
                 length: 17,
-                content: [
-                    {
-                        type: 'declaration',
-                        start: 3,
-                        length: 17
-                    }
-                ]
+                next: {
+                    type: 'declaration',
+                    start: 20,
+                    length: 4,
+                    next: null
+                }
             }
         }
-    ]
+    }
 };
--- test/fixtures/parser/selector-simple.js
+++ test/fixtures/parser/selector-simple.js
@@ -2,28 +2,27 @@ module.exports = {
     type: 'stylesheet',
     start: 0,
     length: 8,
-    content: [
-        {
-            type: 'ruleset',
+    content: {
+        type: 'ruleset',
+        start: 0,
+        length: 8,
+        next: null,
+
+        selector: {
+            type: 'selector',
             start: 0,
-            length: 8,
-            selector: {
-                type: 'selector',
-                start: 0,
-                length: 2
-            },
-            block: {
-                type: 'block',
+            length: 2
+        },
+        block: {
+            type: 'block',
+            start: 3,
+            length: 4,
+            content: {
+                type: 'declaration',
                 start: 3,
                 length: 4,
-                content: [
-                    {
-                        type: 'declaration',
-                        start: 3,
-                        length: 4
-                    }
-                ]
+                next: null
             }
         }
-    ]
+    }
 };
